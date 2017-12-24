84b1043be4b492a3eba4e8fcf10429242fe0d8ad
LEJYONER <yusufhamza59@hotmail.com>
2017-12-25 01:02:35 +0300

    added some commands

--- NettyBaseReloaded/Game/netty/commands/new_client/DroneFormationChangeCommand.cs
+++ NettyBaseReloaded/Game/netty/commands/new_client/DroneFormationChangeCommand.cs
@@ -1,7 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NettyBaseReloaded.Utils;
+
 namespace NettyBaseReloaded.Game.netty.commands.new_client
 {
-    public class DroneFormationChangeCommand
+    class DroneFormationChangeCommand
     {
-        
+        public const short ID = 32691;
+
+        public static Command write(int userId, int selectedFormationId)
+        {
+            var cmd = new ByteArray(ID);
+            cmd.Short(-10056);
+            cmd.Integer(selectedFormationId << 12 | selectedFormationId >> 20);
+            cmd.Integer(userId << 13 | userId >> 19);
+            return new Command(cmd.ToByteArray(), true);
+        }
     }
 }
--- NettyBaseReloaded/Game/netty/commands/old_client/CompanyHierarchyRankingModule.cs
+++ NettyBaseReloaded/Game/netty/commands/old_client/CompanyHierarchyRankingModule.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NettyBaseReloaded.Utils;
 
 namespace NettyBaseReloaded.Game.netty.commands.old_client
 {
@@ -10,5 +11,38 @@ namespace NettyBaseReloaded.Game.netty.commands.old_client
     {
         public const short ID = 29039;
 
+        public int clanId = 0;
+      
+        public int rank = 0;
+      
+        public string clanName = "";
+      
+        public string leaderName = "";
+      
+        public string cbsNamesAndLocations = "";
+      
+        public int rankingPoints = 0;
+      
+        public CompanyHierarchyRankingModule(int clanId, int rank, string clanName, string leaderName, string cbsNamesAndLocations, int rankingPoints)
+        {
+            this.clanId = clanId;
+            this.rank = rank;
+            this.clanName = clanName;
+            this.leaderName = leaderName;
+            this.cbsNamesAndLocations = cbsNamesAndLocations;
+            this.rankingPoints = rankingPoints;
+        }
+
+        public byte[] write()
+        {
+            var cmd = new ByteArray(ID);
+            cmd.Integer(this.clanId);
+            cmd.Integer(this.rank);
+            cmd.UTF(this.clanName);
+            cmd.UTF(this.leaderName);
+            cmd.UTF(this.cbsNamesAndLocations);
+            cmd.Integer(this.rankingPoints);
+            return cmd.Message.ToArray();
+        }
     }
 }
