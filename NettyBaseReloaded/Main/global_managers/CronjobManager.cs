53a9baabbfe2ff2cf89fba298abde1e8a0e184f5
Ice Shock <ic3.sh0ck@gmail.com>
2018-01-23 06:57:30 +0200

    total crons

---
+++ NettyBaseReloaded/Main/global_managers/CronjobManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NettyBaseReloaded.Main.objects;
+
+namespace NettyBaseReloaded.Main.global_managers
+{
+    class CronjobManager
+    {
+        public List<ICron> Cronjobs = new List<ICron>();
+
+        public void GetAllCrons()
+        {
+            
+        }
+    }
+}
