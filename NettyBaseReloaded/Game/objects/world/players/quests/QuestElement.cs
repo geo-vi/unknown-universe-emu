d65284835f8b48a5abdca7db0deed5a235f1405f
Ice Shock <ic3.sh0ck@gmail.com>
2018-01-29 14:13:25 +0200

    started addint lootmodule

--- NettyBaseReloaded/Game/objects/world/players/quests/QuestElement.cs
+++ NettyBaseReloaded/Game/objects/world/players/quests/QuestElement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NettyBaseReloaded.Game.netty.commands.old_client;
+using NettyBaseReloaded.Game.objects.world.characters;
 
 namespace NettyBaseReloaded.Game.objects.world.players.quests
 {
@@ -20,6 +20,14 @@ namespace NettyBaseReloaded.Game.objects.world.players.quests
                 questElements.Add(new netty.commands.old_client.QuestElementModule(new netty.commands.old_client.QuestCaseModule(questElement.Case.Id, questElement.Case.Active, questElement.Case.Mandatory, questElement.Case.Ordered, questElement.Case.MandatoryCount, ParseElementsOld(questElement.Case.Elements)), null));
             }
             return questElements;
+        }
+
+        public static List<netty.commands.old_client.LootModule> ParseRewardsOld(Reward rewards)
+        {
+            foreach (var reward in rewards.Rewards)
+            {
+                
+            }
         }
 
         //TODO: Add new
