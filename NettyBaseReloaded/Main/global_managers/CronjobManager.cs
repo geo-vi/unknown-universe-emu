6f5fc8532e93c7ed27494abeaa0480a1a77aaf88
Ice Shock <ic3.sh0ck@gmail.com>
2018-01-31 22:45:13 +0200

    started adding crons

--- NettyBaseReloaded/Main/global_managers/CronjobManager.cs
+++ NettyBaseReloaded/Main/global_managers/CronjobManager.cs
@@ -1,19 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using NettyBaseReloaded.Main.interfaces;
 using NettyBaseReloaded.Main.objects;
 
 namespace NettyBaseReloaded.Main.global_managers
 {
-    class CronjobManager
+    class CronjobManager : ITick
     {
-        public List<ICron> Cronjobs = new List<ICron>();
+        public List<Cronjob> Cronjobs = new List<Cronjob>();
 
         public void GetAllCrons()
         {
-            
+            if (!File.Exists(Directory.GetCurrentDirectory() + "/crons.xml")) return;
+            var xml = XDocument.Load(Directory.GetCurrentDirectory() + "/crons.xml");
+            foreach (var cronjob in xml.Descendants())
+            {
+                
+            }
+        }
+
+        public void Tick()
+        {
+            foreach (var cron in Cronjobs)
+            {
+                cron.Tick();
+            }
         }
     }
 }
---
+++ NettyBaseReloaded/Main/objects/Cronjob.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace NettyBaseReloaded.Main.objects
+{
+    class Cronjob
+    {
+        public string Name { get; set; }
+
+        public DateTime ExecutionTime { get; set; }
+
+        public bool RepeatedTask { get; set; }
+
+        public int Intervals { get; set; }
+
+        public string ExecuteStr { get; set; }
+
+        public void Execute()
+        {
+            if (ExecuteStr.StartsWith("run"))
+            {
+                var path = ExecuteStr.Split('-', '>');
+                Console.WriteLine(path[1]);
+            }
+
+            if (!RepeatedTask)
+            {
+                Remove();
+            }
+        }
+
+        public static Cronjob ParseCronjob(XmlDocument xml)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Tick()
+        {
+            if (ExecutionTime.AddMinutes(Intervals) < DateTime.Now) Execute();
+        }
+
+        public void Remove()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
